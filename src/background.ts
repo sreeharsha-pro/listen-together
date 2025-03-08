e9231a6abe422381a8220e526d6aeab290a3a155
Sreeharsha D <sreeharsha.pro@gmail.com>
2025-03-09 01:25:30 +0530

    Updated event listeners with more checks,  -> Added a env and modified actions to be based on time changed even  play and pause

--- src/background.ts
+++ src/background.ts
@@ -5,29 +5,31 @@ let ws: WebSocket | null = null;
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log("📨 Received message from content script:", message);
   
-    // Forward the message to WebSocket server
+  // Forward the message to WebSocket server
+  console.log(ws,ws?.readyState, WebSocket.OPEN)
     if (ws && ws.readyState === WebSocket.OPEN) {
       ws.send(JSON.stringify(message));
     }
 });
   
-
 function sendMessageToTabs(message: { event: string; currentTime?: number }) {
+    console.log("send message to tabs function called")
     chrome.tabs.query({}, (tabs) => {
       for (const tab of tabs) {
         if (tab.id) {
           chrome.tabs.sendMessage(tab.id, message);
+          console.log("Forwarding message to content script in tab: ", tab.id)
         }
       }
     });
   }
 
 function connectWebSocket() {
-  ws = new WebSocket("ws://localhost:8080");
+  ws = new WebSocket(`ws://localhost:${process.env.PORT}`);
 
   ws.onopen = () => console.log("✅ Connected to WebSocket server");
 
-  ws.onerror = (error) => console.error("❌ WebSocket Error:", error);
+  ws.onerror = (error) => console.error("❌ WebSocket Error:", JSON.stringify(error));
 
   ws.onclose = () => {
     console.warn("⚠️ WebSocket Disconnected. Reconnecting in 3s...");
@@ -35,8 +37,9 @@ function connectWebSocket() {
   };
 
   ws.onmessage = (event) => {
-    console.log("📩 Received from WebSocket:", event.data);
+    console.log("📩 Background script onmessage received from WebSocket:", event.data);
     const message = JSON.parse(event.data);
+    console.log(message)
     sendMessageToTabs(message);
   };
 }
